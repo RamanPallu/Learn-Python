6e5446577cd73b55f252d205d80921112574acd3
Ramana_Pallu <56391732+RamanPallu@users.noreply.github.com>
2024-05-08 16:53:23 +0000

    even

---
+++ src/sparkcode.py
@@ -0,0 +1,8 @@
+# Define a list of numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Filter even numbers using list comprehension
+even_numbers = [num for num in numbers if num % 2 == 0]
+
+# Print the even numbers
+print("Even numbers in the list:", even_numbers)
